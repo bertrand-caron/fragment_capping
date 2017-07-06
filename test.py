2ae561066f333a21869073e000b59431fc448665
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-07-07 09:59:54 +1000

    Feature: Move dihedral_fragments related code back to dihedral_fragments module

--- test.py
+++ test.py
@@ -1,10 +1,72 @@
-from fragment_capping.molecule_for_fragment import molid_after_capping_fragment
+from fragment_capping.helpers.types_helpers import Atom
+from fragment_capping.helpers.molecule import Molecule
 
-def test_cyclic_fragments():
-    print(molid_after_capping_fragment('C,H,H|C|C|C,H,H|000'))
-    print(molid_after_capping_fragment('C,H,H|C|C|C,H,H|010'))
-    print(molid_after_capping_fragment('C,H,H|C|C|C,H,H|020'))
-    print(molid_after_capping_fragment('C,H,H|C|C|C,H,H|030'))
+def example_1() -> None:
+    uncapped_molecule = Molecule(
+        {
+            1: Atom(index=1, element='C', valence=3, capped=False, coordinates=None),
+            2: Atom(index=2, element='C', valence=3, capped=False, coordinates=None),
+        },
+        [
+            (1, 2),
+        ]
+    )
+
+    uncapped_molecule.write_graph('uncapped_molecule')
+
+    capped_molecule = uncapped_molecule.get_best_capped_molecule()
+
+    capped_molecule.write_graph('capped_molecule')
+
+    print(capped_molecule.dummy_pdb())
+
+def example_2() -> None:
+    uncapped_molecule = Molecule(
+        {
+            1: Atom(index=1, element='C', valence=3, capped=False, coordinates=None),
+            2: Atom(index=2, element='O', valence=1, capped=False, coordinates=None),
+        },
+        [
+            (1, 2),
+        ]
+    )
+
+    uncapped_molecule.write_graph('uncapped_molecule')
+
+    capped_molecule = uncapped_molecule.get_best_capped_molecule()
+
+    capped_molecule.write_graph('capped_molecule')
+
+    print(capped_molecule.dummy_pdb())
+
+def example_3() -> None:
+    uncapped_molecule = Molecule(
+        {
+            1: Atom(index=1, element='C', valence=3, capped=False, coordinates=None),
+            2: Atom(index=2, element='C', valence=3, capped=False, coordinates=None),
+            3: Atom(index=3, element='C', valence=3, capped=False, coordinates=None),
+            4: Atom(index=4, element='C', valence=3, capped=False, coordinates=None),
+            5: Atom(index=5, element='C', valence=3, capped=False, coordinates=None),
+            6: Atom(index=6, element='C', valence=3, capped=False, coordinates=None),
+        },
+        [
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+            (5, 6),
+            (6, 1),
+        ]
+    )
+
+    uncapped_molecule.write_graph('uncapped_molecule')
+
+    capped_molecule = uncapped_molecule.get_best_capped_molecule()
+
+    capped_molecule.write_graph('capped_molecule')
+
+    print(capped_molecule.dummy_pdb())
 
 if __name__ == '__main__':
-    test_cyclic_fragments()
+    for example in [example_1, example_2, example_3]:
+        example()
