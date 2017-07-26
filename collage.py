ff8fbbd47c48abc4208dd48c8522d6b5d178b5c5
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-07-26 16:03:13 +1000

    Feature: Address some issues with (not) returning tuples for indices_for_subplot()

--- collage.py
+++ collage.py
@@ -38,15 +38,18 @@ def best_grid(l: int, aspect_ratio: Tuple[Numeric, Numeric] = (1, 1)) -> Tuple[i
 
     return sorted_grids[0]
 
-def indices_for_subplot(n: int, subplot_dims: Tuple[int, int]) -> Union[Tuple[int, int], int]:
+def indices_for_subplot(n: int, subplot_dims: Tuple[int, int], always_return_tuple: bool = False) -> Union[Tuple[int, int], int]:
     indices = (
         n // subplot_dims[LINES],
         n - (n // subplot_dims[LINES]) * subplot_dims[LINES],
     )
 
-    if subplot_dims[COLUMNS] == 0:
+    assert 0 <= indices[COLUMNS] < subplot_dims[COLUMNS]
+    assert 0 <= indices[LINES] < subplot_dims[LINES]
+
+    if subplot_dims[COLUMNS] == 1 and not always_return_tuple:
         return indices[LINES]
-    elif subplot_dims[LINES] == 0:
+    elif subplot_dims[LINES] == 1 and not always_return_tuple:
         return indices[COLUMNS]
     else:
         return indices
