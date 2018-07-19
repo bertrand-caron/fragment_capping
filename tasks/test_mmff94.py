f5850b8f8492d19a9990beadeec74f415cf3a0aa
Bertrand Caron <bertrand.fx.caron@gmail.com>
2018-07-19 13:46:50 +1000

    Feature: Use graph isomorphism for matching results; add charge constraints (default inactive)

--- tasks/test_mmff94.py
+++ tasks/test_mmff94.py
@@ -3,11 +3,15 @@ from argparse import ArgumentParser, Namespace
 from itertools import groupby
 from operator import itemgetter
 from datetime import datetime
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Optional
 from os.path import join, exists
+from re import match
 
 from fragment_capping.helpers.molecule import molecule_from_mol2_str
 from fragment_capping.helpers.rings import bonds_for_ring
+from fragment_capping.helpers.graphs import lewis_graph, are_graphs_isomorphic
+
+CONSTRAINT_TOTAL_CHARGE = False
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -85,6 +89,8 @@ if __name__ == '__main__':
     with open('MMFF94/MMFF94_hypervalent.mol2') as fh:
         all_mol2_str = [('@<TRIPOS>MOLECULE' + mol2_str) for mol2_str in fh.read().split('@<TRIPOS>MOLECULE')[1:]]
 
+    assert len(all_mol2_str) == 761, len(all_mol2_str)
+
     args = parse_args()
 
     if args.all:
@@ -106,19 +112,40 @@ if __name__ == '__main__':
             try_plotting_molecule(mol2_str)
             try:
                 molecule = molecule_from_mol2_str(mol2_str)
+            except KeyboardInterrupt:
+                raise
             except Exception as e:
                 print(mol2_str)
                 print(str(e))
                 status[molecule_name(mol2_str)] = WRONG_MOL2
                 continue
+
+            if CONSTRAINT_TOTAL_CHARGE:
+                net_charge = int(
+                    round(
+                        sum(
+                            float(line.split()[8])
+                            for line in mol2_str.splitlines()
+                            if len(line.split()) == 9 and match('^ *[0-9]+ ', line) is not None
+                        ),
+                    )
+                )
+            else:
+                net_charge = None
+
+            molecule.net_charge = net_charge
+            old_molecule = deepcopy(molecule)
             old_bond_orders = deepcopy(molecule.bond_orders)
             _, graph, pos = molecule.write_graph('RAW', graph_kwargs=dict(include_atom_index=True))
+
 
             try:
                 t1 = datetime.now()
                 molecule.assign_bond_orders_and_charges_with_ILP()
                 timing = datetime.now() - t1
                 timings[molecule.name] = (len(molecule.atoms), len(molecule.bonds), timing)
+            except KeyboardInterrupt:
+                raise
             except Exception as e:
                 print('--SOLVER ERROR--')
                 print(molecule, molecule.net_charge)
@@ -130,32 +157,49 @@ if __name__ == '__main__':
             new_bond_orders = deepcopy(molecule.bond_orders)
             molecule.write_graph('ILP', g=graph, pos=pos, graph_kwargs=dict(include_atom_index=True))
 
-            def assert_bond_orders_match() -> None:
-                assert old_bond_orders == new_bond_orders, (
+            def assert_bond_orders_match(e: Optional['AssertionError']) -> None:
+                assert are_graphs_isomorphic(
+                    [
+                        lewis_graph(
+                            molecule,
+                            # Remove non-bonded electrons since the reference do not have them
+                            use_non_bonded_electrons=False,
+                        )
+                        for molecule in  (old_molecule, molecule)
+                    ],
+                ), (
                     molecule.name,
                     {bond: (old_bond_orders[bond], new_bond_orders[bond]) for bond in old_bond_orders.keys() if old_bond_orders[bond] != new_bond_orders[bond]},
                 )
-                status[molecule.name] = SAME_BOND_ORDER
+                if e is None:
+                    status[molecule.name] = SAME_BOND_ORDER + '0' + ',' + '-'
+                else:
+                    status[molecule.name] = SAME_BOND_ORDER + str(len(e.args[0][1]))  + ',' + '0'
 
             try:
-                assert_bond_orders_match()
+                assert_bond_orders_match(None)
             except AssertionError as e:
                 print('SOFT FAIL (AROMATIC EQUIVALENCE)')
                 first_bond = lambda ring: bonds_for_ring(ring)[0]
                 try:
                     molecule.assign_aromatic_bonds()
+                    bond_order_constraints = [
+                        (first_bond(ring), old_bond_orders[first_bond(ring)])
+                        for ring in molecule.aromatic_rings
+                    ]
+                    if False:
+                        print(f'PDB: {molecule.pdb()}')
+                        print(f'aromatic_rings: {molecule.aromatic_rings}')
+                        print(f'bond_order_constraints: {bond_order_constraints}')
                     # Rerun solver with constraining bond order of one bond of each aromatic ring
                     molecule.assign_bond_orders_and_charges_with_ILP(
-                        bond_order_constraints=[
-                            (first_bond(ring), old_bond_orders[first_bond(ring)])
-                            for ring in molecule.aromatic_rings
-                        ],
+                        bond_order_constraints=bond_order_constraints,
                     )
                     new_bond_orders = deepcopy(molecule.bond_orders)
-                    assert_bond_orders_match()
-                except AssertionError as e:
+                    assert_bond_orders_match(e)
+                except AssertionError as f:
                     print(str(e))
-                    status[molecule.name] = DIFFERENT_BOND_ORDER
+                    status[molecule.name] = DIFFERENT_BOND_ORDER + str(len(e.args[0][1])) + ',' + str(len(f.args[0][1]))
     finally:
         get_molecule_name, on_status = itemgetter(0), itemgetter(1)
         grouped = {
@@ -172,3 +216,4 @@ if __name__ == '__main__':
         print(grouped)
         assert sum(grouped.values()) == len(test_mol2_str), (sum(grouped.values()), len(test_mol2_str))
         plot_timings(timings)
+        print(status)
